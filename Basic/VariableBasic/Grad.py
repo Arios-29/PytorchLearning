7de21d7a1aaa4771ff89e60a462b0af95611b3a8
Arios-29 <2508335092@qq.com>
2021-11-19 00:03:52 +0800

    求导

--- Basic/VariableBasic/Grad.py
+++ Basic/VariableBasic/Grad.py
@@ -7,7 +7,7 @@ pytorch只能用标量来求导
 """
 
 # 标量对向量求导
-# 本质是y=f(x1, x2,...,xn),对x=(x1, x2,...,xn)求到后得到一个向量,i位置对应y对xi的导数
+# 本质是y=f(x1, x2,...,xn),对x=(x1, x2,...,xn)求导后得到一个向量,i位置对应y对xi的导数
 vec_x = Variable(torch.rand(4), requires_grad=True)
 w = Variable(torch.tensor([1.0, 1.0, 1.0, 1.0]), requires_grad=True)
 y = (vec_x * w).sum()  # 本质是 y = w.t().mm(vec_x)
@@ -15,8 +15,20 @@ y.backward()
 print(vec_x.grad)
 print(w.grad)
 
-# 向量对向量求导
-vec_x.grad.data.zero_()
-z = vec_x * w
-z.backward(torch.tensor([1, 2, 3, 4]))  # 1*f1' + 2*f2' + 3*f3' + 4*f4' , fi'表示z[i-1]对x的导数
-print(vec_x.grad)
+# 标量对矩阵求导
+# 本质还是y=f(x11, x12,...,xmn),对x求导后得到一个矩阵, ij位置对应y对xij的导数
+matrix_x = Variable(torch.rand((4, 4)), requires_grad=True)
+y = matrix_x ** 2
+y = y.sum()
+y.backward()
+print(matrix_x.grad)
+
+# backward()中的gradient参数
+# z=f(y), y=g(x), 则z.backward()等价于y.backward(gradient=grad_y), grad_y为z对y求导, grad_y的形状与y一致
+# grad(z, xij)=grad(z, y1)*grad(y1, xij)+...+grad(z, ym)*grad(ym, xij) 对应x.grad中ij位置
+x = Variable(torch.rand((4, 4)), requires_grad=True)
+y = x * 2.0
+z = y.mean()
+grad_y = Variable(torch.ones((4, 4))*0.0625)
+y.backward(gradient=grad_y)
+print(x.grad)
